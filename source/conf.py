a8abcecaad96166543f2a7891c17d2b467ad01b2
murnana <murnana@users.noreply.github.com>
2020-05-16 21:15:20 +0900

    add recommonmark

--- source/conf.py
+++ source/conf.py
@@ -31,6 +31,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'recommonmark',
     'nbsphinx',
     'sphinx.ext.mathjax',
 ]
